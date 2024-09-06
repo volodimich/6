7c16225dbc4968769cc24c36116b02d4cf302771
volodimich <burittosforme@gmail.com>
2024-09-06 14:59:14 +0700

    Add files via upload

---
+++ lesson6.py
@@ -0,0 +1,9 @@
+immutable_var = 1, 3, 8, False, 'sigma'
+print(immutable_var)
+immutable_var = 1, 2, 7, True, 'omega', [1, 2, 3, 4]
+immutable_var [5] [2] = 4
+print(immutable_var)
+# кортеж, это неизменяемый тип данных
+mutable_list = [1, 2, 3, 4, 'sigma', True]
+mutable_list [1] = 'Vova'
+print(mutable_list)
